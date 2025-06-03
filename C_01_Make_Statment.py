70604a113d69bfb9c5feb6e536208839d2971cda
Kieran Borthwick <Kieranborthwick@mmc.school.nz>
2025-06-04 10:52:21 +1200

    C_01_Make_Statment.py

--- C_01_Make_Statment.py
+++ C_01_Make_Statment.py
@@ -1,11 +1,8 @@
-# Functions go here
+# Function goes here
 def make_statement(statement, decoration):
     """Emphasises headings by adding decoration at the start and end"""
-
     print(f"{decoration * 3} {statement} {decoration * 3}")
 
 
-# Main Routine goes here
-make_statement(statement="Instructions", decoration="ℹ️")
-
-
+# Main routine starts here
+make_statement("Instructions", "ℹ️")

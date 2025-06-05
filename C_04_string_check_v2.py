beaff16de503b951f289737fd783f0422e0944bc
Kieran Borthwick <Kieranborthwick@mmc.school.nz>
2025-06-05 12:47:10 +1200

    C_04_string_check_v2.py

--- C_04_string_check_v2.py
+++ C_04_string_check_v2.py
@@ -1,29 +1,26 @@
 # Functions go here
 def string_check(question, valid_ans_list, num_letters):
-    """Checks that users enter the full word or the first letter of a word
-    from a list of valid responses"""
+    """
+    Checks that users enter the full word or the first 'n' letters of a valid response.
+    """
 
     while True:
-        response = input(question).lower()
+        response = input(question).strip().lower()
 
         for item in valid_ans_list:
-
-            # Checks if the response is the entire word
-            if response == item:
+            # Check for full word match or valid shortcut
+            if response == item or response == item[:num_letters]:
                 return item
 
-            # Checks if it's the first letter
-            elif response == item[:num_letters]:
-                return item
-
-        print(f"Please choose a option from {valid_ans_list}")
+        print(f"Please choose an option from {valid_ans_list}.\n")
 
 
 # Main routine goes here
 yes_no_list = ['yes', 'no']
 payment_list = ['cash', 'credit']
 
-like_coffee = string_check("Do you like coffee? ", yes_no_list, 1)
-print(f"You chose {like_coffee}")
-pay_method = string_check("Payment method: ", payment_list, 2)
-print(f"You chose {pay_method}")
+like_coffee = string_check("Do you like coffee? (yes/no): ", yes_no_list, 1)
+print(f"You chose: {like_coffee}")
+
+pay_method = string_check("Payment method (cash/credit): ", payment_list, 2)
+print(f"You chose: {pay_method}")

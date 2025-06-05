67837d0a16a7c356902c91659d784d771372e541
Kieran Borthwick <Kieranborthwick@mmc.school.nz>
2025-06-05 12:46:14 +1200

    C_04_string_check_v1.py

--- C_04_string_check_v1.py
+++ C_04_string_check_v1.py
@@ -1,28 +1,26 @@
 # Functions go here
 def string_check(question, valid_ans_list):
-    """Checks that users enter the full word or the first letter of a word
-    from a list of valid responses"""
+    """
+    Checks that users enter either the full word or the first letter of a word
+    from a list of valid responses.
+    """
 
     while True:
-        response = input(question).lower()
+        response = input(question).strip().lower()
 
         for item in valid_ans_list:
-
-            # Checks if the response is the entire word
-            if response == item:
+            # Checks if the response is the full word or first letter
+            if response == item or response == item[0]:
                 return item
 
-            # Checks if it's the first letter
-            elif response == item[0]:
-                return item
-
-        print(f"Please choose a option from {valid_ans_list}")
+        print(f"Please choose an option from {valid_ans_list}.\n")
 
 
 # Main routine goes here
 levels = ['easy', 'medium', 'hard']
 
-like_coffee = string_check("Do you like coffee? ", ['yes', 'no'])
-print(f"You chose {like_coffee}")
-choose_level = string_check("Choose a level: ", levels)
-print(f"You chose {choose_level}")
+like_coffee = string_check("Do you like coffee? (yes/no): ", ['yes', 'no'])
+print(f"You chose: {like_coffee}")
+
+choose_level = string_check("Choose a level (easy/medium/hard): ", levels)
+print(f"You chose: {choose_level}")
